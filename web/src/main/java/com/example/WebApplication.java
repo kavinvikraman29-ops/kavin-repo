253ddeb872a8b6c31f3355b46ec17d48cda53658
kavin <kavinvikraman29@gmail.com>
2026-04-22 15:05:27 +0800

    init commit

--- web/src/main/java/com/example/WebApplication.java
+++ web/src/main/java/com/example/WebApplication.java
@@ -8,12 +8,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
-    }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
     }
 }
